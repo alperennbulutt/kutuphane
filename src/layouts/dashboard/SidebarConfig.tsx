afc3f3ea2177000d6f105c200ddedc49b9fe2c7e
alperennbulutt <alperenbulut.ab@gmail.com>
2021-05-05 22:26:40 +0300

    first commit

---
+++ src/layouts/dashboard/SidebarConfig.tsx
@@ -0,0 +1,90 @@
+// routes
+import { PATH_DASHBOARD } from '../../routes/paths';
+// components
+import SvgIconStyle from '../../components/SvgIconStyle';
+
+// ----------------------------------------------------------------------
+
+const getIcon = (name: string) => (
+  <SvgIconStyle
+    src={`/static/icons/navbar/${name}.svg`}
+    sx={{ width: 22, height: 22 }}
+  />
+);
+
+const ICONS = {
+  map: getIcon('ic_map'),
+  blog: getIcon('ic_blog'),
+  cart: getIcon('ic_cart'),
+  chat: getIcon('ic_chat'),
+  mail: getIcon('ic_mail'),
+  page: getIcon('ic_page'),
+  user: getIcon('ic_user'),
+  copy: getIcon('ic_copy'),
+  error: getIcon('ic_error'),
+  charts: getIcon('ic_charts'),
+  editor: getIcon('ic_editor'),
+  upload: getIcon('ic_upload'),
+  animate: getIcon('ic_animate'),
+  calendar: getIcon('ic_calendar'),
+  elements: getIcon('ic_elements'),
+  carousel: getIcon('ic_carousel'),
+  language: getIcon('ic_language'),
+  ecommerce: getIcon('ic_ecommerce'),
+  analytics: getIcon('ic_analytics'),
+  dashboard: getIcon('ic_dashboard'),
+  components: getIcon('ic_components'),
+  authenticator: getIcon('ic_authenticator')
+};
+
+const sidebarConfig = [
+  // GENERAL
+  // ----------------------------------------------------------------------
+  {
+    subheader: 'Sayfalar',
+    items: [
+      {
+        title: 'Anlık Turnike Hareketleri',
+        href: PATH_DASHBOARD.general.turnstile,
+        icon: ICONS.dashboard
+      },
+      {
+        title: 'Ziyaretçi İstatistikleri',
+        href: PATH_DASHBOARD.general.analytics,
+        icon: ICONS.ecommerce
+      },
+      {
+        title: 'Çalışma Alanları',
+        href: PATH_DASHBOARD.general.ecommerce,
+        icon: ICONS.analytics
+      },
+      {
+        title: 'Sessiz Kütüphane',
+        href: PATH_DASHBOARD.user.cards,
+        icon: ICONS.ecommerce
+      },
+      {
+        title: 'Randevular',
+        href: PATH_DASHBOARD.general.ecommerce,
+        icon: ICONS.ecommerce
+      },
+      {
+        title: 'Çalışma Takvimi',
+        href: PATH_DASHBOARD.general.ecommerce,
+        icon: ICONS.ecommerce
+      },
+      {
+        title: 'Masalar',
+        href: PATH_DASHBOARD.general.ecommerce,
+        icon: ICONS.ecommerce
+      },
+      {
+        title: 'Duyurular',
+        href: PATH_DASHBOARD.general.ecommerce,
+        icon: ICONS.editor
+      }
+    ]
+  }
+];
+
+export default sidebarConfig;
---
+++ src/views/kutuphane/Turnstile.tsx
@@ -0,0 +1,3 @@
+export default function Turnstile() {
+  return <div>Turnstile view</div>;
+}
